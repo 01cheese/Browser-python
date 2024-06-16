a2b9bd1b16cffaf7c215204832fd29efd94dc2e9
Vadym Zelenko <115219323+01cheese@users.noreply.github.com>
2024-06-16 14:39:42 +0200

    theme.py

--- theme.py
+++ theme.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QPushButton, QRadioButton, QApplication
-from PyQt5.QtGui import QPalette, QColor
+from PyQt5.QtGui import QPalette, QColor, QFont
 from PyQt5.QtCore import Qt
 
 class ThemeManager:
@@ -47,14 +47,10 @@ class ThemeManager:
     def enable_dark_mode(self):
         self.dark_mode_enabled = True
         self.apply_theme_to_app()
-        for i in range(self.parent.tabs.count()):
-            self.apply_theme_to_page(self.parent.tabs.widget(i))
 
     def disable_dark_mode(self):
         self.dark_mode_enabled = False
         self.apply_theme_to_app()
-        for i in range(self.parent.tabs.count()):
-            self.apply_theme_to_page(self.parent.tabs.widget(i))
 
     def apply_theme_to_app(self):
         if self.dark_mode_enabled:
@@ -74,39 +70,90 @@ class ThemeManager:
             dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
             dark_palette.setColor(QPalette.HighlightedText, Qt.black)
             QApplication.setPalette(dark_palette)
+            QApplication.setStyleSheet(self.dark_qss())
         else:
             QApplication.setStyle("Fusion")
             QApplication.setPalette(QApplication.style().standardPalette())
+            QApplication.setStyleSheet("")
 
-    def apply_theme_to_page(self, browser):
-        if self.dark_mode_enabled:
-            browser.page().runJavaScript("""
-                (function() {
-                    var css = 'html, body { background-color: #333; color: #ccc; } a { color: #42a2da; }';
-                    var head = document.head || document.getElementsByTagName('head')[0];
-                    var style = document.createElement('style');
-                    style.type = 'text/css';
-                    if (style.styleSheet) {
-                        style.styleSheet.cssText = css;
-                    } else {
-                        style.appendChild(document.createTextNode(css));
-                    }
-                    head.appendChild(style);
-                })();
-            """)
-        else:
-            browser.page().runJavaScript("""
-                (function() {
-                    var css = 'html, body { background-color: #fff; color: #000; } a { color: #1a0dab; }';
-                    var head = document.head || document.getElementsByTagName('head')[0];
-                    var style = document.createElement('style');
-                    style.type = 'text/css';
-                    if (style.styleSheet) {
-                        style.styleSheet.cssText = css;
-                    } else {
-                        style.appendChild(document.createTextNode(css));
-                    }
-                    head.appendChild(style);
-                })();
-            """)
-
+    def dark_qss(self):
+        return """
+        QMainWindow {
+            background-color: #353535;
+        }
+        QMenuBar {
+            background-color: #444444;
+            color: #ffffff;
+        }
+        QMenuBar::item {
+            background: #444444;
+            color: #ffffff;
+        }
+        QMenuBar::item:selected {
+            background: #666666;
+        }
+        QMenu {
+            background-color: #444444;
+            color: #ffffff;
+        }
+        QMenu::item {
+            background-color: #444444;
+            color: #ffffff;
+        }
+        QMenu::item:selected {
+            background-color: #666666;
+        }
+        QTabBar::tab {
+            background: #555555;
+            color: #ffffff;
+            padding: 5px;
+        }
+        QTabBar::tab:selected {
+            background: #777777;
+        }
+        QToolBar {
+            background: #444444;
+        }
+        QToolBar QToolButton {
+            background: #555555;
+            color: #ffffff;
+            border: none;
+            padding: 5px;
+        }
+        QToolBar QToolButton:hover {
+            background: #666666;
+        }
+        QStatusBar {
+            background: #444444;
+            color: #ffffff;
+        }
+        QLineEdit {
+            background: #555555;
+            color: #ffffff;
+            border: 1px solid #777777;
+        }
+        QProgressBar {
+            background: #555555;
+            color: #ffffff;
+            border: 1px solid #777777;
+        }
+        QSlider::groove:horizontal {
+            height: 6px;
+            background: #555555;
+        }
+        QSlider::handle:horizontal {
+            background: #777777;
+            border: 1px solid #444444;
+            width: 18px;
+            margin: -6px 0;
+        }
+        QPushButton {
+            background-color: #555555;
+            color: #ffffff;
+            border: none;
+            padding: 5px;
+        }
+        QPushButton:hover {
+            background-color: #777777;
+        }
+        """
