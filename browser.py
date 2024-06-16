9efa166eb6fcaa4dc180c03124d8891a663c3862
Vadym Zelenko <115219323+01cheese@users.noreply.github.com>
2024-06-16 14:43:10 +0200

    browser.py

--- browser.py
+++ browser.py
@@ -3,10 +3,13 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
-
 from theme import ThemeManager
 from bookmarks import BookmarkManager
 from functools import partial
+import favicon
+import asyncio
+import threading
+
 
 class Browser(QMainWindow):
     def __init__(self):
@@ -22,7 +25,10 @@ class Browser(QMainWindow):
         self.theme_manager = ThemeManager(self)
         self.bookmark_manager = BookmarkManager(self)
 
+        self.downloads = []
+
         self.create_navbar()
+        self.create_bookmarks_bar()
         self.create_status_bar()
         self.create_menu()
 
@@ -58,13 +64,17 @@ class Browser(QMainWindow):
         self.url_bar.returnPressed.connect(self.navigate_to_url)
         navbar.addWidget(self.url_bar)
 
-        self.progress = QProgressBar()
-        self.progress.setMaximum(100)
-        navbar.addWidget(self.progress)
+        bookmark_btn = QAction(QIcon("icons/bookmark.png"), "Bookmark", self)
+        bookmark_btn.triggered.connect(self.bookmark_page)
+        navbar.addAction(bookmark_btn)
 
         self.downloads_btn = QAction(QIcon("icons/download.png"), "Downloads", self)
         self.downloads_btn.triggered.connect(self.show_downloads)
         navbar.addAction(self.downloads_btn)
+
+        self.progress = QProgressBar()
+        self.progress.setMaximum(100)
+        navbar.addWidget(self.progress)
 
         self.zoom_slider = QSlider(Qt.Horizontal)
         self.zoom_slider.setMinimum(25)
@@ -75,9 +85,27 @@ class Browser(QMainWindow):
         self.zoom_slider.valueChanged.connect(self.change_zoom)
         navbar.addWidget(self.zoom_slider)
 
-        bookmark_btn = QAction(QIcon("icons/bookmark.png"), "Bookmark", self)
-        bookmark_btn.triggered.connect(self.bookmark_page)
-        navbar.addAction(bookmark_btn)
+    def create_bookmarks_bar(self):
+        self.bookmarks_bar = QToolBar("Bookmarks")
+        self.addToolBar(Qt.TopToolBarArea, self.bookmarks_bar)
+        self.bookmarks_bar.setMovable(False)
+        self.update_bookmarks_bar()
+
+    def update_bookmarks_bar(self):
+        self.bookmarks_bar.clear()
+        for title, url in self.bookmark_manager.bookmarks:
+            icon = self.get_favicon(url)
+            bookmark_btn = QAction(QIcon(icon), title, self)
+            bookmark_btn.triggered.connect(partial(self.add_new_tab, QUrl(url), title))
+            self.bookmarks_bar.addAction(bookmark_btn)
+
+    def get_favicon(self, url):
+        icons = favicon.get(url)
+        icon = icons[0] if icons else None
+        if icon:
+            return QIcon(QPixmap(icon.url))
+        else:
+            return QIcon()
 
     def create_status_bar(self):
         self.status = QStatusBar()
@@ -132,7 +160,7 @@ class Browser(QMainWindow):
         browser.loadProgress.connect(self.update_progress)
         browser.loadFinished.connect(partial(self.update_tab_title, i=i, browser=browser))
 
-        self.theme_manager.apply_theme_to_page(browser)
+        browser.page().profile().downloadRequested.connect(self.on_download_requested)
 
     def tab_open_doubleclick(self, i):
         if i == -1:
@@ -162,7 +190,6 @@ class Browser(QMainWindow):
         if browser != self.tabs.currentWidget():
             return
         self.url_bar.setText(q.toString())
-        self.theme_manager.apply_theme_to_page(browser)
 
     def update_progress(self, progress):
         self.progress.setValue(progress)
@@ -195,37 +222,91 @@ class Browser(QMainWindow):
     def bookmark_page(self):
         current_url = self.tabs.currentWidget().url().toString()
         current_title = self.tabs.currentWidget().page().title()
-        self.bookmark_manager.add_bookmark(current_title, current_url)
+        title, ok = QInputDialog.getText(self, "Bookmark", "Enter title:", text=current_title)
+        if ok and title:
+            self.bookmark_manager.add_bookmark(title, current_url)
+            self.update_bookmarks_bar()
 
     def on_download_requested(self, download):
+        self.downloads.append(download)
+        self.handle_download(download)
+
+    def handle_download(self, download):
         download_path, _ = QFileDialog.getSaveFileName(self, "Save File", download.path())
         if download_path:
             download.setPath(download_path)
             download.accept()
-            dialog = QProgressDialog("Downloading...", "Cancel", 0, 100, self)
-            dialog.setWindowTitle("Download")
-            dialog.setWindowModality(Qt.WindowModal)
-
-            download.downloadProgress.connect(lambda received, total: dialog.setValue(int(received / total * 100)))
-            download.finished.connect(dialog.close)
-            download.stateChanged.connect(lambda state: self.on_download_state_changed(state, dialog))
-
-            dialog.canceled.connect(download.cancel)
-            dialog.show()
+            self.start_download_progress(download, download_path)
         else:
             download.cancel()
 
-    def on_download_state_changed(self, state, dialog):
-        if state == QWebEngineDownloadItem.DownloadCompleted:
-            QMessageBox.information(self, "Download Completed", "File downloaded successfully!")
-        elif state == QWebEngineDownloadItem.DownloadCancelled:
-            QMessageBox.warning(self, "Download Cancelled", "File download was cancelled.")
-        elif state == QWebEngineDownloadItem.DownloadInterrupted:
-            QMessageBox.critical(self, "Download Interrupted", "File download was interrupted.")
+    def start_download_progress(self, download, download_path):
+        dialog = QProgressDialog("Downloading...", "Cancel", 0, 100, self)
+        dialog.setWindowTitle("Download")
+        dialog.setWindowModality(Qt.WindowModal)
+        dialog.setValue(0)
+        dialog.show()
+
+        def progress_callback(received, total):
+            dialog.setValue(int(received / total * 100))
+
+        def state_callback(state):
+            if state == QWebEngineDownloadItem.DownloadCompleted:
+                dialog.close()
+                QMessageBox.information(self, "Download Completed", "File downloaded successfully!")
+            elif state == QWebEngineDownloadItem.DownloadCancelled:
+                dialog.close()
+                QMessageBox.warning(self, "Download Cancelled", "File download was cancelled.")
+            elif state == QWebEngineDownloadItem.DownloadInterrupted:
+                dialog.close()
+                QMessageBox.critical(self, "Download Interrupted", "File download was interrupted.")
+
+        download.downloadProgress.connect(progress_callback)
+        download.stateChanged.connect(state_callback)
+
+        download.downloadProgress.connect(lambda received, total: progress_callback(received, total))
+        download.stateChanged.connect(lambda state: state_callback(state))
+
+    async def download_file(self, download, download_path):
+        dialog = QProgressDialog("Downloading...", "Cancel", 0, 100, self)
+        dialog.setWindowTitle("Download")
+        dialog.setWindowModality(Qt.WindowModal)
+        dialog.setValue(0)
+        dialog.show()
+
+        def progress_callback(received, total):
+            dialog.setValue(int(received / total * 100))
+
+        def state_callback(state):
+            if state == QWebEngineDownloadItem.DownloadCompleted:
+                dialog.close()
+                QMessageBox.information(self, "Download Completed", "File downloaded successfully!")
+            elif state == QWebEngineDownloadItem.DownloadCancelled:
+                dialog.close()
+                QMessageBox.warning(self, "Download Cancelled", "File download was cancelled.")
+            elif state == QWebEngineDownloadItem.DownloadInterrupted:
+                dialog.close()
+                QMessageBox.critical(self, "Download Interrupted", "File download was interrupted.")
+
+        download.downloadProgress.connect(progress_callback)
+        download.stateChanged.connect(state_callback)
+
+        while download.state() in [QWebEngineDownloadItem.DownloadRequested, QWebEngineDownloadItem.DownloadInProgress]:
+            await asyncio.sleep(0.1)
 
     def show_downloads(self):
-        # Show the downloads manager or history here
-        QMessageBox.information(self, "Downloads", "This feature is not implemented yet.")
+        downloads_dialog = QDialog(self)
+        downloads_dialog.setWindowTitle("Downloads")
+        layout = QVBoxLayout()
+
+        for download in self.downloads:
+            download_info = f"{download.path().split('/')[-1]} - {download.state()}"
+            label = QLabel(download_info)
+            layout.addWidget(label)
+
+        downloads_dialog.setLayout(layout)
+        downloads_dialog.exec_()
+
 
 app = QApplication(sys.argv)
 QApplication.setApplicationName("My Browser")
